fba49e82d430b1ccff88244a6993373648eac04e
Marti Victor <vmarti@z6po.enseeiht.fr>
2023-01-23 09:45:20 +0100

    b

--- pod-api/etape1/Server.java
+++ pod-api/etape1/Server.java
@@ -6,37 +6,47 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.rmi.server.UnicastRemoteObject;
 
-public class Server implements Server_itf{
-    
+public class Server extends UnicastRemoteObject implements Server_itf {
+
     private ArrayList<ServerObject> server_objects;
 
     private HashMap<String, Integer> table;
 
-    public int lookup(String name) throws java.rmi.RemoteException{
+    protected Server() throws RemoteException {
+        server_objects = new ArrayList<ServerObject>();
+        table = new HashMap<String, Integer>();
+    }
+
+    public int lookup(String name) throws RemoteException {
         return table.get(name);
     }
-	public void register(String name, int id) throws java.rmi.RemoteException{
+
+    public void register(String name, int id) throws RemoteException {
         table.put(name, id);
     }
 
-	public int create(Object o) throws java.rmi.RemoteException{
-        int id = o.hashCode(); //il est possible qu'un hashcode ne soit pas unique néanmoins c'est très peu probable
+    public int create(Object o) throws RemoteException {
+        int id = o.hashCode(); // il est possible qu'un hashcode ne soit pas unique néanmoins c'est très peu
+                               // probable
         server_objects.add(new ServerObject(o, id));
         return id;
     }
-	public Object lock_read(int id, Client_itf client) throws java.rmi.RemoteException{
-        return null;
-    }
-	public Object lock_write(int id, Client_itf client) throws java.rmi.RemoteException{
+
+    public Object lock_read(int id, Client_itf client) throws RemoteException {
         return null;
     }
 
-    public static void main(String[] args) throws MalformedURLException, RemoteException, AlreadyBoundException{
+    public Object lock_write(int id, Client_itf client) throws RemoteException {
+        return null;
+    }
+
+    public static void main(String[] args) throws MalformedURLException, RemoteException, AlreadyBoundException {
         try {
-            LocateRegistry.createRegistry(4000);
+            Registry registre = LocateRegistry.createRegistry(4000);
         } catch (Exception e) {
         }
-        Naming.bind("//localhost:4000/server", new Server());
+        Naming.bind("//localhost:8080/server", new Server());
     }
 }
